c3d55b7556205f310d73bf9e12c1c0487719c1ea
Michelle Sauder <45767844+michellesauder@users.noreply.github.com>
2019-07-04 11:26:43 -0700

    Added HTML, CSS and SASS code

---
+++ app/data/events.js
@@ -0,0 +1,227 @@
+let eventData = [
+    
+    { "start":"10-Sep-18", "title":"Orientation / Study Skills\n\n", "room":360},
+    { "start":"11-Sep-18", "title":"Team Building / C# Intro\n\n", "room":360},
+    { "start":"12-Sep-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"13-Sep-18", "title":"Introductory Java Programming\n\nPaul Mills", "room":360},
+    { "start":"14-Sep-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    
+    
+    { "start":"17-Sep-18", "title":"Relational Databases\n\nPhil Weier", "room":382},
+    { "start":"18-Sep-18", "title":"Systems Analysis and Design\n\nPat McGee", "room":382},
+    { "start":"19-Sep-18", "title":"OO Programming in C#\n\nPhil Weier", "room":382},
+    { "start":"20-Sep-18", "title":"Introductory Java Programming\n\nPaul Mills", "room":382},
+    { "start":"21-Sep-18", "title":"Systems Analysis and Design\n\nPat McGee", "room":382},
+    
+    
+    { "start":"24-Sep-18", "title":"Relational Databases\n\nPhil Weier", "room":360},
+    { "start":"25-Sep-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"26-Sep-18", "title":"OO Programming in C#\n\nPhil Weier", "room":360},
+    { "start":"27-Sep-18", "title":"Introductory Java Programming\n\nPaul Mills", "room":360},
+    { "start":"28-Sep-18", "title":"Systems Analysis and Design\n\nPat McGee", "room":360},
+    
+
+    { "start":"01-Oct-18", "title":"Relational Databases\n\nPhil Weier", "room":382},
+    { "start":"02-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"03-Oct-18", "title":"OO Programming in C#\n\nPhil Weier", "room":382},
+    { "start":"04-Oct-18", "title":"Introductory Java Programming\n\nPaul Mills", "room":382},
+    { "start":"05-Oct-18", "title":"OO Programming in C#\n\nPhil Weier", "room":382},
+    
+    
+    { "start":"08-Oct-18", "title":"Thanksgiving Day\n\n", "room":0},
+    { "start":"09-Oct-18", "title":"Systems Analysis and Design\n\nPat McGee", "room":360},
+    { "start":"10-Oct-18", "title":"Relational Databases\n\nPhil Weier", "room":360},
+    { "start":"11-Oct-18", "title":"Introductory Java Programming\n\nPaul Mills", "room":360},
+    { "start":"12-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    
+    
+    { "start":"15-Oct-18", "title":"OO Programming in C#\n\nPhil Weier", "room":382},
+    { "start":"16-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"17-Oct-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    { "start":"18-Oct-18", "title":"Relational Databases\n\nPhil Weier", "room":382},
+    { "start":"19-Oct-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    
+    
+    { "start":"22-Oct-18", "title":"OO Programming in C# \n\nPhil Weier", "room":360},
+    { "start":"23-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"24-Oct-18", "title":"Relational Databases\n\nPhil Weier", "room":360},
+    { "start":"25-Oct-18", "title":"OO Programming in C#\n\nPhil Weier", "room":360},
+    { "start":"26-Oct-18", "title":"Flex day (no class is currently scheduled but this could change).\n\n", "room":360},
+    
+    
+    { "start":"29-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"30-Oct-18", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"31-Oct-18", "title":"C# LINQ and Windows Forms\n\nPat McGee", "room":382},
+    { "start":"01-Nov-18", "title":"Presentation Skills\n\nMarlene Delanghe", "room":382},
+    { "start":"02-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    
+    
+    { "start":"05-Nov-18", "title":"C# LINQ and Windows Forms\n\nPat McGee", "room":360},
+    { "start":"06-Nov-18", "title":"JavaScript\n\nJason Harrison", "room":360},
+    { "start":"07-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":360},
+    { "start":"08-Nov-18", "title":"C# LINQ and Windows Forms\n\nPat McGee", "room":360},
+    { "start":"09-Nov-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":360},
+    
+    
+    { "start":"12-Nov-18", "title":"Remembrance Day - BCIT Closed\n\n", "room":0},
+    { "start":"13-Nov-18", "title":"JavaScript\n\nJason Harrison", "room":382},
+    { "start":"14-Nov-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":382},
+    { "start":"15-Nov-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":382},
+    { "start":"16-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    
+    
+    { "start":"19-Nov-18", "title":"C# LINQ and Windows Forms\n\nPat McGee", "room":360},
+    { "start":"20-Nov-18", "title":"JavaScript\n\nJason Harrison", "room":360},
+    { "start":"21-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":360},
+    { "start":"22-Nov-18", "title":"Android Introduction\n\nPaul Mills", "room":360},
+    { "start":"23-Nov-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":360},
+    
+    
+    { "start":"26-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    { "start":"27-Nov-18", "title":"JavaScript\n\nJason Harrison", "room":382},
+    { "start":"28-Nov-18", "title":"Angular & React.js\n\nPat McGee", "room":382},
+    { "start":"29-Nov-18", "title":"Android Introduction\n\nPaul Mills", "room":382},
+    { "start":"30-Nov-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":382},
+    
+    
+    { "start":"03-Dec-18", "title":"Project 1 - Phase 1\n\nPat McGee", "room":360},
+    { "start":"04-Dec-18", "title":"JavaScript\n\nJason Harrison", "room":360},
+    { "start":"05-Dec-18", "title":"Project 1 - Phase 1\n\nPat McGee", "room":360},
+    { "start":"06-Dec-18", "title":"Android Introduction\n\nPaul Mills", "room":360},
+    { "start":"07-Dec-18", "title":"ASP.NET – MVC\n\nPhil Weier", "room":360},
+    
+    
+    { "start":"10-Dec-18", "title":"Project 1 - Phase 1\n\nPat McGee", "room":382},
+    { "start":"11-Dec-18", "title":"JavaScript\n\nJason Harrison", "room":382},
+    { "start":"12-Dec-18", "title":"Project 1 - Phase 1\n\nPat McGee", "room":382},
+    { "start":"13-Dec-18", "title":"Android Introduction\n\nPaul Mills", "room":382},
+    { "start":"14-Dec-18", "title":"Flex day (no class is currently scheduled but this could change).\n\n", "room":382},
+    { "start":"", "title":"Christmas Break: December 16 - January 7\n\n", "room":0},
+    
+    
+    { "start":"07-Jan-19", "title":"ASP.NET – MVC\n\nPhil Weier", "room":360},
+    { "start":"08-Jan-19", "title":"ASP.NET – Web API\n\nPat McGee", "room":360},
+    { "start":"09-Jan-19", "title":"ASP.NET – MVC\n\nPhil Weier", "room":360},
+    { "start":"10-Jan-19", "title":"Android Introduction\n\nPaul Mills", "room":360},
+    { "start":"11-Jan-19", "title":"OO PHP & Laravel\n\nTBA", "room":360},
+    
+    
+    { "start":"14-Jan-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"15-Jan-19", "title":"OO PHP & Laravel\n\nTBA", "room":382},
+    { "start":"16-Jan-19", "title":"ASP.NET – Web API\n\nPat McGee", "room":382},
+    { "start":"17-Jan-19", "title":"Android Introduction\n\nPaul Mills", "room":382},
+    { "start":"18-Jan-19", "title":"OO PHP & Laravel\n\nTBA", "room":382},
+    
+    
+    { "start":"21-Jan-19", "title":"ASP.NET – Web API\n\nPat McGee", "room":360},
+    { "start":"22-Jan-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"23-Jan-19", "title":"Web Application Security\n\nPat McGee", "room":360},
+    { "start":"24-Jan-19", "title":"Android Introduction\n\nPaul Mills", "room":360},
+    { "start":"25-Jan-19", "title":"OO PHP & Laravel\n\nTBA", "room":360},
+    
+    
+    { "start":"28-Jan-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":382},
+    { "start":"29-Jan-19", "title":"ASP.NET – Web API\n\nPat McGee", "room":382},
+    { "start":"30-Jan-19", "title":"Web Application Security\n\nPat McGee", "room":382},
+    { "start":"31-Jan-19", "title":"Android Introduction\n\nPaul Mills", "room":382},
+    { "start":"01-Feb-19", "title":"OO PHP & Laravel\n\nTBA", "room":382},
+    
+    
+    { "start":"04-Feb-19", "title":"Web Application Security\n\nPat McGee", "room":360},
+    { "start":"05-Feb-19", "title":"OO PHP & Laravel\n\nTBA", "room":360},
+    { "start":"06-Feb-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":360},
+    { "start":"07-Feb-19", "title":"ASP.NET – Web API\n\nPat McGee", "room":360},
+    { "start":"08-Feb-19", "title":"Flex day (no class is currently scheduled but this could change).\n\n", "room":360},
+    
+    
+    { "start":"11-Feb-19", "title":"Family Day\n\n", "room":382},
+    { "start":"12-Feb-19", "title":"IOS Development\n\nPhil Weier", "room":382},
+    { "start":"13-Feb-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":382},
+    { "start":"14-Feb-19", "title":"OO PHP & Laravel\n\nTBA", "room":382},
+    { "start":"15-Feb-19", "title":"Web Application Security\n\nPat McGee", "room":382},
+    
+    
+    { "start":"18-Feb-19", "title":"Web Application Security\n\nPat McGee", "room":360},
+    { "start":"19-Feb-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"20-Feb-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":360},
+    { "start":"21-Feb-19", "title":"IOS Development\n\nPhil Weier", "room":360},
+    { "start":"22-Feb-19", "title":"OO PHP & Laravel\n\nTBA", "room":360},
+    
+    
+    { "start":"25-Feb-19", "title":"IOS Development\n\nPhil Weier", "room":382},
+    { "start":"26-Feb-19", "title":"Project 1 - Phase 2\n\nPat McGee", "room":382},
+    { "start":"27-Feb-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":382},
+    { "start":"28-Feb-19", "title":"Project 1 - Phase 2\n\nPat McGee", "room":382},
+    { "start":"01-Mar-19", "title":"AWS Intro\n\nPat McGee", "room":382},
+    
+    
+    { "start":"04-Mar-19", "title":"IOS Development\n\nPhil Weier", "room":360},
+    { "start":"05-Mar-19", "title":"Project 1 - Phase 2\n\nPat McGee", "room":360},
+    { "start":"06-Mar-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":360},
+    { "start":"07-Mar-19", "title":"IOS Development\n\nPhil Weier", "room":360},
+    { "start":"08-Mar-19", "title":"AWS Intro\n\nPat McGee", "room":360},
+    
+    
+    { "start":"11-Mar-19", "title":"IOS Development\n\nPhil Weier", "room":382},
+    { "start":"12-Mar-19", "title":"Project 1 - Phase 2\n\nPat McGee", "room":382},
+    { "start":"13-Mar-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":382},
+    { "start":"14-Mar-19", "title":"AWS Intro\n\nPat McGee", "room":382},
+    { "start":"15-Mar-19", "title":"Project 1 - Phase 3\n\nPat McGee", "room":382},
+    
+    
+    { "start":"18-Mar-19", "title":"March Break\n\n", "room":0},
+    { "start":"19-Mar-19", "title":"March Break\n\n", "room":0},
+    { "start":"20-Mar-19", "title":"March Break\n\n", "room":0},
+    { "start":"21-Mar-19", "title":"March Break\n\n", "room":0},
+    { "start":"22-Mar-19", "title":"March Break\n\n", "room":0},
+    
+    
+    { "start":"25-Mar-19", "title":"Resume Writing Essentials\n\n Marlene Delanghe ", "room":360},
+    { "start":"26-Mar-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"27-Mar-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":360},
+    { "start":"28-Mar-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    { "start":"29-Mar-19", "title":"BCIT Closed Good F\n\n", "room":0},
+    
+    
+    { "start":"01-Apr-19", "title":"BCIT Closed Easter M\n\n", "room":0},
+    { "start":"02-Apr-19", "title":"Project 1 - Phase 3\n\nPat McGee", "room":382},
+    { "start":"03-Apr-19", "title":"Project 1 - Phase 3\n\nPat McGee", "room":382},
+    { "start":"04-Apr-19", "title":"IOS Development\n\nPhil Weier", "room":382},
+    { "start":"05-Apr-19", "title":"Project 1 - Phase 3\n\nPat McGee", "room":382},
+    
+    
+    { "start":"08-Apr-19", "title":"Interview Skills\n\n Marlene Delanghe ", "room":360},
+    { "start":"09-Apr-19", "title":"Mock Interviews\n\n Marlene Delanghe/Phil/Pat ", "room":360},
+    { "start":"10-Apr-19", "title":"MEAN (Mongo/ExpressJS/Angular\n\node)\n\nMedhat Elmasry", "room":360},
+    { "start":"11-Apr-19", "title":"Flex day (no class is currently scheduled but this could change).\n\n", "room":360},
+    { "start":"12-Apr-19", "title":"Front End Web Design/Scripting/Tooling\n\nMichael Whyte", "room":360},
+    
+    
+    { "start":"15-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"16-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"17-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"18-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"19-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    
+    
+    { "start":"22-Apr-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"23-Apr-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"24-Apr-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"25-Apr-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"26-Apr-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    
+    
+    { "start":"29-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"30-Apr-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"01-May-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"02-May-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    { "start":"03-May-19", "title":"Industry Project\n\nPat McGee", "room":382},
+    
+    
+    { "start":"06-May-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"07-May-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"08-May-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"09-May-19", "title":"Industry Project\n\nPat McGee", "room":360},
+    { "start":"10-May-19", "title":"Industry Project\n\nPat McGee", "room":360}
+    
+]
